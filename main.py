56a7d9d4b0d5f5e9d2c39f80f956d3d32f1be101
henriquecscode <henriquecostasousa.hcs@gmail.com>
2019-11-28 21:43:23 +0000

    Refactored code to a single main file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
-from config import win_width, win_height, max_fps
-from classes import Taz, Game
+from config import win_width, win_height, max_fps, line_height, line_total_height, line_width
 
 
 def init():
@@ -11,34 +10,175 @@ def init():
 
     return win, clock
 
+class Movable:
+    min_x = win_width / 2 - line_width / 2 - 20
+    max_x = win_width /2 - line_width / 2 + 20
+    min_y = win_height / 2 - line_total_height / 2
 
-def loop(win, clock):
-    win.fill((0, 0, 0))
+    width = 30
+    height = 30
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+    velocity = 10
+    def __init__(self, win, x, y, color, direction=1, difficulty=1):
+        self.win = win
+        self.x = x
+        self.y = y
+        self.direction = direction
+        self.difficulty = difficulty
+        self.color = color
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        taz.move_left()
-    if keys[pygame.K_RIGHT]:
-        taz.move_right()
-    if keys[pygame.K_UP]:
-        taz.move_up()
-    if keys[pygame.K_DOWN]:
-        taz.move_down()
+    def move(self):
+        self.x += self.direction * self.velocity * self.difficulty ** 0.5
 
-    taz.loop()
-    game.loop()
-    pygame.display.flip()
+        return self.x < self.min_x or self.x > self.max_x
+        # If the return value is true we can delete the object
+
+    def draw(self):
+        pygame.draw.rect(self.win, self.color, (self.x - self.width / 2, self.y - self.height / 2), (self.width, self.height))
+
+    def loop(self):
+        self.draw()
+        return self.move()
+
+class Obstacle(Movable):
+
+    color = (100, 100, 100)
+
+    def __init__(self, win, x, y, direction, difficulty):
+        super().__init__(win, x, y, self.color, direction, difficulty)
+
+class Reward(Movable):
+
+    color = (0, 100, 100)
+    
+    def __init__(self, win, x, y, direction, difficulty):
+        super().__init__(win, x, y, self.color, direction, difficulty)
+    
+
+class Taz:
+    min_x = win_width / 2 - line_width / 2
+    max_x = win_width / 2 + line_width / 2
+    min_y = win_height / 2 - line_total_height / 2
+    max_y = win_height / 2 + line_total_height / 2
+
+    line_height = line_height
+
+    velocity = 10
+    width = 30
+    height = 30
+    color = (255, 0, 0)
+
+    # So it doesn't automatically jump lines
+    up_thres = 10
+    down_thres = 10
+
+    def __init__(self, win, clock):
+        self.win = win
+        self.x = win_width / 2
+        self.y = win_height / 2
+        self.clock = clock
+
+        self.up_request = 0
+        self.down_request = 0
+
+    def move_up(self):
+        self.up_request += 1
+        self.down_request = 0
+        if self.up_request >= self.up_thres:
+            self.up_request = 0
+            if self.y - self.line_height >= self.min_y:
+                self.y -= self.line_height
+
+    def move_down(self):
+        self.down_request += 1
+        self.up_request = 0
+        if self.down_request >= self.down_thres:
+            self.down_request = 0
+            if self.y + self.line_height < self.max_y:
+                self.y += self.line_height
+
+    def move_left(self):
+        if self.x - self.velocity >= self.min_x:
+            self.x -= self.velocity
+
+    def move_right(self):
+        if self.x + self.velocity <= self.max_x:
+            self.x += self.velocity
+
+    def draw(self):
+        pygame.draw.rect(self.win, self.color, ((
+            self.x - self.width/2, self.y - self.height/2), (self.width, self.height)))
+
+    def loop(self):
+        self.draw()
+
+
+class Game:
+
+    lines_number = line_total_height // line_height
+    min_x = win_width / 2 - line_width / 2 - 20
+    max_x = win_width / 2 + line_width / 2 + 20
+    min_y = win_height / 2 - line_total_height / \
+        2 + 16  # Shift of 18 to lay beneath taz
+    line_height = line_height
+    obstacles = []
+    rewards = []
+
+    color = (255, 0, 0)
+
+    def __init__(self, win, clock):
+        self.win = win
+        self.clock = clock
+        self.font = pygame.font.SysFont('comicsans', 30, True) #(font, size, bold, italicized)
+        self.taz = Taz(win, clock)
+
+    def draw(self):
+        for i in range(self.lines_number):
+            pygame.draw.line(self.win, self.color, (self.min_x, self.min_y + i * self.line_height),
+                             (self.max_x, self.min_y + i * self.line_height))
+
+    def loop(self):   
+        while True:
+            self.clock.tick(max_fps)
+            self.win.fill((0, 0, 0))
+
+            self.events()
+
+            text =self.font.render(str(pygame.time.get_ticks()), 1, (255, 255, 255))
+            self.win.blit(text, (0,0))
+            self.loop_rewards()
+            self.loop_obstacles()
+            self.taz.loop()
+            self.draw()
+
+            pygame.display.flip()
+
+    def events(self):
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.taz.move_left()
+        if keys[pygame.K_RIGHT]:
+            self.taz.move_right()
+        if keys[pygame.K_UP]:
+            self.taz.move_up()
+        if keys[pygame.K_DOWN]:
+            self.taz.move_down()
+
+    # Could probably join these two, semantically it is cleaner this way though
+    def loop_rewards(self):
+        self.rewards = [x for x in self.rewards if loop(x)]
+    
+    def loop_obstacles(self):
+        self.obstacles = [x for x in self.obstacles if loop(x)]
+        # Simultaneously draws, moves and deletes the object if need be
+
 
 
 win, clock = init()
-taz = Taz(win, clock)
 game = Game(win, clock)
-while True:
-    clock.tick(max_fps)
-    loop(win, clock)
-
-# pygame.quit()
+game.loop()
