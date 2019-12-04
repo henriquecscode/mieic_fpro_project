adb5ba8a7dd02a9150a42ec0ff565a11c9aca363
henriquecscode <henriquecostasousa.hcs@gmail.com>
2019-12-04 09:06:01 +0000

    Added assets

--- main.py
+++ main.py
@@ -10,6 +10,34 @@ def init():
 
     return win, clock
 
+class Spritesheet(object): # https://www.pygame.org/wiki/Spritesheet
+    def __init__(self, filename):
+        try:
+            self.sheet = pygame.image.load(filename).convert()
+        except pygame.error as message:
+            print ('Unable to load spritesheet image:' + message)
+            raise SystemExit
+    # Load a specific image from a specific rectangle
+    def image_at(self, rectangle, colorkey = None):
+        "Loads image from x,y,x+offset,y+offset"
+        rect = pygame.Rect(rectangle)
+        image = pygame.Surface(rect.size).convert()
+        image.blit(self.sheet, (0, 0), rect)
+        if colorkey is not None:
+            if colorkey is -1:
+                colorkey = image.get_at((0,0))
+            image.set_colorkey(colorkey, pygame.RLEACCEL)
+        return image
+    # Load a whole bunch of images and return them as a list
+    def images_at(self, rects, colorkey = None):
+        "Loads multiple images, supply a list of coordinates" 
+        return [self.image_at(rect, colorkey) for rect in rects]
+    # Load a whole strip of images
+    def load_strip(self, rect, image_count, colorkey = None):
+        "Loads a strip of images and returns them as a list"
+        tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
+                for x in range(image_count)]
+        return self.images_at(tups, colorkey)
 
 class Movable:
     min_x = win_width / 2 - line_width / 2 - 20
@@ -179,10 +207,10 @@ class Game:
         # Probable reason: Child cannot access the self that is passed in the function defined here so it is set to None
         # Best solution? Pass self.scenes and self.scene to the child
         # It is only a reference so it is not that heavy in terms of performance I guess
-        scenes = self.scenes
-        scene = self.scene
         def scene_changer(name):
-            self.scene = self.scenes[name]
+            scene = self.scene
+            scenes = self.scenes
+            scene = scenes[name]
         return scene_changer(name) 
 
     def test(self):
